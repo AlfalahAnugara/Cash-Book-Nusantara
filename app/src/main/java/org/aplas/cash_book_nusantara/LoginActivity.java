3b887fdae5765c651dcd6d66b6f7545a930c520d
alfalahanugara <alfalah190299@gmail.com>
2021-10-26 19:45:10 +0700

    Menambahkan Deklarasi Object, Listener untuk button, Mengambil value, dan Intent ketika kita berhasil login maka akan langsung direct ke halaman Home/Beranda

--- app/src/main/java/org/aplas/cash_book_nusantara/LoginActivity.java
+++ app/src/main/java/org/aplas/cash_book_nusantara/LoginActivity.java
@@ -1,5 +1,6 @@
 package org.aplas.cash_book_nusantara;
 
+import android.accounts.Account;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,42 +11,40 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText username, password ;
-    Button loginbutton ;
-    DBHelper DB ;
+    EditText idUsername, idPassword;
+    Button loginButton ;
+
+    DBHelper dbhelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
-        loginbutton = (Button) findViewById(R.id.loginbutton);
-        DB = new DBHelper(this) ;
+        dbhelper = new DBHelper(getApplicationContext());
 
-        loginbutton.setOnClickListener(new View.OnClickListener() {
+        idUsername = (EditText)findViewById(R.id.idUsername);
+        idPassword = (EditText)findViewById(R.id.idPassword);
+        loginButton = (Button)findViewById(R.id.loginButton);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
+            public void onClick(View v) {
+                String username = idUsername.getText().toString();
+                String password = idPassword.getText().toString();
 
-                if(user.equals("")|pass.equals(""))
-                    Toast.makeText(LoginActivity.this, "Please enter all the fields" ,Toast.LENGTH_SHORT).show();
-                else{
-                    Boolean checkuserpass = DB.checkusernamenamepassword(user,pass);
-                    if(checkuserpass==true) {
-                        Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), org.aplas.cash_book_nusantara.HomeActivity.class);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(LoginActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
-                    }
+                Account account = dbhelper.login(username, password);
+                if(account != null){
+                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    intent.putExtra("account", account);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(LoginActivity.this, "Username or Password Doesn't Match", Toast.LENGTH_SHORT).show();
                 }
-
-
-
             }
         });
+
     }
 }
